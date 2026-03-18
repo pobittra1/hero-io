cdead022cffa9504550e769a2dc8879ee81fa5e6
pobittra chandra roy <devpobittra@gmail.com>
2026-03-18 23:18:40 +0600

    show animation when every time navigate

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import { useEffect, useState } from "react";
@@ -6,6 +6,13 @@ import { Circles } from "react-loader-spinner";
 
 const Root = () => {
     const [loading, setLoading] = useState(true);
+    const navigation = useNavigation();
+
+    const loaderTemplate = (
+        <div className="flex justify-center items-center h-screen">
+            <Circles height="80" width="80" color="#3b82f6" />
+        </div>
+    )
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -17,16 +24,19 @@ const Root = () => {
 
     if (loading) {
         return (
-            <div className="flex justify-center items-center h-screen">
-                <Circles height="80" width="80" color="#3b82f6" />
-            </div>
+            loaderTemplate
         );
     }
 
     return (
         <div className="max-w-300 m-auto bg-gray-50 shadow-sm text-gray-800">
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            {navigation.state === "loading" ? (
+                loaderTemplate
+            ) : (
+                <Outlet />
+            )}
+
             <Footer></Footer>
         </div>
     );
