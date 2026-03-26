893e41d1d0cd82c86a43f6f81c4e77bbbeb0186c
pobittra chandra roy <devpobittra@gmail.com>
2026-03-26 11:43:14 +0600

    refactor page not found page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,20 +1,18 @@
+import pageNotFound from "../../assets/error-404.png";
 const ErrorPage = () => {
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center px-4">
-            <div className="text-center animate-fadeIn">
-                <h1 className="text-9xl font-extrabold text-primary mb-4">404</h1>
+            <div className="text-center animate-fadeIn p-8">
+                <img src={pageNotFound} alt="Page Not Found !" />
                 <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gray-800">
-                    Oops! Page Not Found
+                    Oops, Page Not Found!
                 </h2>
                 <p className="text-gray-600 mb-6 max-w-md mx-auto">
-                    Sorry, the page you are looking for does not exist or has been moved.
+                    The page your are looking for in not availabe.
                 </p>
-                <div className="flex justify-center gap-4">
+                <div>
                     <a href="/" className="btn btn-primary btn-lg">
-                        Go Home
-                    </a>
-                    <a href="/contact" className="btn btn-outline btn-lg">
-                        Contact Support
+                        Go Back!
                     </a>
                 </div>
             </div>
